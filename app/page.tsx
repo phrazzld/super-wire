4aab97b8ade285c268950ecd107a1d877f73ee9d
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-02-14 19:13:27 -0600

    Add loading state

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,14 @@ import { storage } from "../pages/_app";
 import { getDownloadURL, ref } from "firebase/storage";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
   const [episodes, setEpisodes] = useState<any[]>([]);
 
   // TODO: Hook
   useEffect(() => {
     fetch("/api/episodes")
       .then((res) => res.json())
-      .then((data) =>
+      .then((data) => {
         setEpisodes(
           data.episodes.sort((a: any, b: any) => {
             // Extract the date from the name
@@ -21,9 +22,13 @@ export default function Home() {
             // Compare the dates
             return new Date(bDate).getTime() - new Date(aDate).getTime();
           })
-        )
-      )
-      .catch((err) => console.error(err));
+        );
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
   }, []);
 
   useEffect(() => {
@@ -75,35 +80,40 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-purple-500 via-blue-500 to-teal-500">
-      <h1 className="text-white text-5xl font-bold mb-20">Super Wire</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-screen-lg">
-        {episodes.map((file) => (
-          <div
-            key={file.name}
-            className="rounded-lg shadow-lg overflow-hidden bg-gray-100"
-          >
-            <div className="h-64 flex items-center justify-center px-8">
-              <div>
-                <p className="text-gray-800 font-bold text-2xl">
-                  {formatDate(file.name.replace("-episode.mp3", ""))}
-                </p>
-                <p className="text-gray-700 font-medium text-lg mt-2">
-                  {formatTime(file.name.replace("-episode.mp3", ""))}
-                </p>
-              </div>
-            </div>
-            <audio id={file.name} className="w-full my-2" controls></audio>
-          </div>
-        ))}
-      </div>
-
+      <h1 className="text-white text-5xl font-bold my-20">Super Wire</h1>
       {process.env.NODE_ENV === "development" && (
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full mt-10"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full mb-10"
           onClick={generateEpisode}
         >
           New Episode
         </button>
+      )}
+      {loading ? (
+        <h1 className="text-4xl text-white">Loading...</h1>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-screen-lg">
+            {episodes.map((file) => (
+              <div
+                key={file.name}
+                className="rounded-lg shadow-lg overflow-hidden bg-gray-100"
+              >
+                <div className="h-64 flex items-center justify-center px-8">
+                  <div>
+                    <p className="text-gray-800 font-bold text-2xl">
+                      {formatDate(file.name.replace("-episode.mp3", ""))}
+                    </p>
+                    <p className="text-gray-700 font-medium text-lg mt-2">
+                      {formatTime(file.name.replace("-episode.mp3", ""))}
+                    </p>
+                  </div>
+                </div>
+                <audio id={file.name} className="w-full my-2" controls></audio>
+              </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
